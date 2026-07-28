df7b469fb6d3c9b09b4f61ecca2b8e4878470ce1
malikat <chawqimalika3@gmail.com>
2026-07-28 13:47:09 +0100

    re all

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Initialize MariaDB data directory on first run
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# Read secrets from files (mounted via Docker secrets/_FILE pattern)
+MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
+MYSQL_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+
+# Create the socket/runtime directory MariaDB needs before it can start
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+# Only initialize the data directory if it hasn't been done before
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
 # Run SQL commands in bootstrap mode to create DB and users
 mysqld --user=mysql --bootstrap <<EOF
@@ -13,5 +23,5 @@ GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_RO
 FLUSH PRIVILEGES;
 EOF
 
-# Start MariaDB as PID 1 in foreground
+# Start MariaDB as PID 1 in foreground so it receives signals correctly
 exec mysqld --user=mysql
